3b9f2b4933e35ca8e74c91223efce9b78f12f560
Kevin K <mungaikamau7@gmail.com>
2019-02-02 09:03:18 +0300

    upgraded to clojure 1.9

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.2"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 ;; https://mvnrepository.com/artifact/org.jsoup/jsoup
+                 [org.jsoup/jsoup "1.11.3"]
+                 ]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component.repl "0.2.0"]]
                    :source-paths ["dev"]}})
